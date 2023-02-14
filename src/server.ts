1fc2d4e4d2b8546043c555f392e81b7f215737bd
Pablo Almeida <pablloa51@gmail.com>
2023-02-13 22:10:49 -0300

    aleração do cors

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://chatwoot-whats.web.app/",
+    origin: "*",
   })
 );
 app.use(express.urlencoded({ extended: false }));
